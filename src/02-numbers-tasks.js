abf763df008edfa492920afd09fc6f5a90637def
Slava Torchik <torhik_slava@mail.ru>
2020-05-09 14:54:18 +0300

    feat: find angle between vectors

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -112,8 +112,12 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const product = x1 * x2 + y1 * y2;
+  const module1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const module2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cos = product / (module1 * module2);
+  return Math.acos(cos);
 }
 
 /**
