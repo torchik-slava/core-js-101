2f7cdbd6808c522510727cf26728c8cbd146ec2d
Slava Torchik <torhik_slava@mail.ru>
2020-05-09 15:31:03 +0300

    feat: find angle between clock hands

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -106,8 +106,15 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const m = date.getMinutes();
+  let h = date.getUTCHours();
+  if (h > 12) h %= 12;
+  const mAngle = 6 * m;
+  const hAngle = 0.5 * (60 * h + m);
+  let angle = (mAngle >= hAngle ? mAngle - hAngle : hAngle - mAngle);
+  if (angle > 180) angle -= 180;
+  return angle * (Math.PI / 180);
 }
 
 
